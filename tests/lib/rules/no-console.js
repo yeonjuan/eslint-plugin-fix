e8bd1f3844b8e482a32396151ad2093f767f753c
yeonjuan <yeonjuan93@naver.com>
2020-03-08 02:03:30 +0900

    test

--- tests/lib/rules/no-console.js
+++ tests/lib/rules/no-console.js
@@ -16,20 +16,21 @@ const rule = require('../../../lib/rules/no-console');
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
+const error = { message: 'I can fix it', type: 'MemberExpression' };
 ruleTester.run('no-console', rule, {
   valid: [
     { code: "a = console.log('a');" },
     { code: "foo(console.log('a'));" },
+    { code: "console.log('a') + 1;" },
+    { code: "a = console.warn('a');" },
+    { code: "foo(console.warn('a'));" },
+    { code: "console.warn('a') + 1;" },
   ],
-
   invalid: [
     {
       code: "console.log('a')",
       output: '',
-      errors: [{
-        message: 'I can fix it',
-        type: 'MemberExpression',
-      }],
+      errors: [error],
     },
   ],
 });
