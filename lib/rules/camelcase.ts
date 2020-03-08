da8690e8ad39670108c54e01bbdc1e33afad0bd5
yeonjuan <yeonjuan93@naver.com>
2020-03-08 20:10:17 +0900

    feat(camelcase): add new rule camelcase

---
+++ lib/rules/camelcase.ts
@@ -0,0 +1,38 @@
+import utils from '../utils';
+import { Problem } from 'eslint-rule-composer';
+import { Node, Identifier } from 'ast';
+import { isIdentifier } from '../ast-utils';
+
+function snakeToCamel (snake: string): string {
+  return snake.replace(/(\_[a-z])/g, captured => captured.replace('_', '').toUpperCase());
+}
+
+/**
+ * Map the non-fixable problem to fixable problem.
+ *
+ * @param   problem The non-fixable problem from base rule.
+ * @returns Fixable Problem
+ */
+function mapReport(problem: Problem): Problem {
+  return utils.extendsProblem(problem, {
+    fix(fixer) {
+      const {node} = problem;
+      if (isIdentifier(node)) {
+        fixer.replaceText(
+          node,
+          snakeToCamel(node.name)
+        );
+      }
+      return fixer.replaceText(
+        node as Node,
+        snakeToCamel(((node as Node) as Identifier).name)
+      );
+    },
+  });
+}
+
+export default utils.mapBaseRule(
+  'camelcase',
+  mapReport,
+  'code',
+);
---
+++ test/lib/rules/camelcase.ts
@@ -0,0 +1,21 @@
+import { RuleTester } from 'eslint';
+import rule from '../../../lib/rules/camelcase';
+
+const ruleTester = new RuleTester();
+const error = { message: 'I can fix it', type: 'Identifier' };
+ruleTester.run('camelcase', rule, {
+  valid: [
+  ],
+  invalid: [
+    {
+      code: "var my_favorite_color = 5;",
+      output: "var myFavoriteColor = 5;",
+      errors: [error]
+    },
+    {
+      code: "my_favorite_color = 5;",
+      output: "myFavoriteColor = 5;",
+      errors: [error]
+    },
+  ],
+});
